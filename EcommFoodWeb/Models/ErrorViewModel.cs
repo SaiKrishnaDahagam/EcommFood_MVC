988318c748c7a695d9dc38d54393717c92f990fa
user <user@DESKTOP-USGP18U>
2023-07-24 10:22:10 +0530

    Add project files.

---
+++ EcommFoodWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommFoodWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
